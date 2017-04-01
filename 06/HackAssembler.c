80b0c1e5459aa7b38fc8a42e10677b0756311892
chichunchen <chichunchen844@gmail.com>
2017-04-01 22:26:20 +0800

    Finish first pass.

--- 06/HackAssembler.c
+++ 06/HackAssembler.c
@@ -4,7 +4,7 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-#define DEFAULT_START 10
+#define DEFAULT_START 16
 #define BUFFERLEN 100
 #define ENTRYNUM  1000
 char buffer[BUFFERLEN];
@@ -17,6 +17,42 @@ struct symbol {      // table entry
 int registerptr;
 int symboltabptr;
 struct symbol symboltab[ENTRYNUM];
+
+void initializer();
+void addEntry(char *label, int address);
+bool contains(char *label);
+
+// initialize the symbol table with all the predefined symbols and
+// their pre-allocated RAM addresses
+void initializer()
+{
+    symboltabptr = 0;       // ptr for symbol table
+    char labels[7][10] = {
+        "SP", "LCL", "ARG", "THIS", "THAT", "SCREEN", "KBD"
+    };
+    int addresses[7] = { 0, 1, 2, 3, 4, 16384, 24576 };
+    int i;
+    for (i = 0; i < 7; i++) {
+        addEntry(labels[i], addresses[i]);
+        symboltabptr++;
+    }
+    // set default registers
+    for (i = 0; i < 16; i++) {
+        char *virtual_label = malloc(sizeof(char) * 4);
+        virtual_label[0] = 'R';
+        if (i < 10) {
+            virtual_label[1] = '0' + i;
+            virtual_label[2] = '\0';
+        } else {
+            virtual_label[1] = '0' + i/10;
+            virtual_label[2] = '0' + i%10;
+            virtual_label[3] = '\0';
+        }
+        addEntry(virtual_label, i);
+    }
+
+    registerptr = DEFAULT_START;
+}
 
 bool contains(char *label)
 {
@@ -31,21 +67,31 @@ bool contains(char *label)
     return false;
 }
 
-void addEntry(char *label)
+void addEntry(char *label, int address)
 {
     strcpy(symboltab[symboltabptr].label, label);
-    symboltab[symboltabptr].address = registerptr;
-    printf("T: %d ", symboltab[symboltabptr].address);
-    printf(", %s\n", symboltab[symboltabptr].label);
+    symboltab[symboltabptr].address = address;
+//     printf("T: %d ", symboltab[symboltabptr].address);
+//     printf(", %s\n", symboltab[symboltabptr].label);
     symboltabptr++;
-    registerptr++;
+    //registerptr++;
+}
+
+int getAddress(char *symbol)
+{
+    int i;
+    for (i = 0; i < symboltabptr; i++) {
+        if (strcmp(symboltab[i].label, symbol) == 0)
+            return symboltab[i].address;
+    }
+    fprintf(stderr, "[getAddress]: cannot found address\n");
+    return -1;
 }
 
 
 int main(int argc, const char *argv[])
 {
-    symboltabptr = 0;       // ptr for symbol table
-    registerptr = DEFAULT_START;
+    initializer();
 
     if (argc != 2)
         printf("usage: ./a.out <filename>\n");
@@ -55,39 +101,69 @@ int main(int argc, const char *argv[])
         size_t linecap = BUFFERLEN;
         int linenum = 0;
         int i;
-        while((i = getline(&lineptr, &linecap, fp)) > 0) {
-            char command[linecap];
-            char *ptr = buffer;
-            while(*ptr == ' ')  // skip whitespace in front of the any text
-                ptr++;
-            if (*ptr == '/')
-                continue;
-            int j = 0;
-            while(j < i) {
-                command[j++] = *ptr++;
-                if (*ptr == ' ' || *ptr == '\n')
-                    break;
-            }
-            command[j] = '\0';
-            if (strlen(command) > 1) {      // above did not filter the newline, and if there is, just skip it
-                if (command[0] == '@') {
-                    if (!isdigit(command[1])) {
-                        char *label = &command[1];
-                        if (!contains(label)) {
-                            addEntry(label);
-                        }
-                    }
+        // parse each linenum label, i.e, the text inside the braces,
+        // into the symboltab
+        while((i = getline(&lineptr, &linecap, fp)) > 0) {      // Pass 1
+            char *ptr = lineptr;
+            char *label = malloc(sizeof(char) * BUFFERLEN);
+            if (*ptr != '(') {
+                if (*lineptr != '\n' && *(lineptr+1) != '\n' && *lineptr != '/') {
+//                     printf("%s ", lineptr);
+//                     printf("linenum %d++\n", linenum);
+                    linenum++;
                 }
-                linenum++;
-                printf("%s\n", command);
+                continue;
+            }
+            else {
+                int i = 0;
+                ptr++; // skip '('
+                while(*ptr && *ptr != '\n' && *ptr != ' ')
+                    label[i++] = *ptr++;
+                if (*(ptr-2) == ')') {    // 2 = (null terminator) + ')'
+                    label[i-2] = '\0';
+                    addEntry(label, linenum);
+                    //printf("%s, linenum: %d\n", label, linenum);
+                }
+                else {
+                    fprintf(stderr, ") is missed\n");
+                    return EXIT_FAILURE;
+                }
             }
         }
+
+        // parse symbolic a-instruction
+//         while((i = getline(&lineptr, &linecap, fp)) > 0) {
+//             char command[linecap];
+//             char *ptr = buffer;
+//             while(*ptr == ' ')  // skip whitespace in front of the any text
+//                 ptr++;
+//             if (*ptr == '/')
+//                 continue;
+//             int j = 0;
+//             while(j < i) {
+//                 command[j++] = *ptr++;
+//                 if (*ptr == ' ' || *ptr == '\n')
+//                     break;
+//             }
+//             command[j] = '\0';
+//             if (strlen(command) > 1) {      // above did not filter the newline, and if there is, just skip it
+//                 if (command[0] == '@') {
+//                     if (!isdigit(command[1])) {
+//                         char *label = &command[1];
+//                         if (!contains(label)) {
+//                             addEntry(label);
+//                         }
+//                     }
+//                 }
+//                 linenum++;
+//                 printf("%s\n", command);
+//             }
+//         }
     }
     int j;
     for (j = 0; j < ENTRYNUM; j++) {
         if (isalnum(symboltab[j].label[0])) {
-            printf("T: %d ", symboltab[j].address);
-            printf(", %s\n", symboltab[j].label);
+            printf("T: %d, %s\n", symboltab[j].address, symboltab[j].label);
         }
     }
     return 0;
