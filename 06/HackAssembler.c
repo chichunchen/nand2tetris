cbd6e6517b48cf7ed208060cf08663622514b90c
chichunchen <chichunchen844@gmail.com>
2017-04-02 00:13:18 +0800

    Pass the test of rect and max.

--- 06/HackAssembler.c
+++ 06/HackAssembler.c
@@ -7,8 +7,15 @@
 #define DEFAULT_START 16
 #define BUFFERLEN 100
 #define ENTRYNUM  24577
+#define BITS_NUM    16
+#define TOK_LEN     16
+#define DESTTAB_LEN 8
+#define COMPTAB_LEN 18
+#define JUMPTAB_LEN 8
+
 char buffer[BUFFERLEN];
 
+enum command { A_COMMAND, C_COMMAND, L_COMMAND };
 struct symbol {      // table entry
     char label[100];
     int address;
@@ -18,9 +25,16 @@ int registerptr;
 int symboltabptr;
 struct symbol symboltab[ENTRYNUM];
 
+// parser module
 void initializer();
+int commandType(char *line, int lim);
 void addEntry(char *label, int address);
 bool contains(char *label);
+int getAddress(char *symbol);
+
+// code module
+char * convertA(unsigned int in, char *out);
+void printTable();
 
 // initialize the symbol table with all the predefined symbols and
 // their pre-allocated RAM addresses
@@ -53,13 +67,31 @@ void initializer()
     registerptr = DEFAULT_START;
 }
 
+int commandType(char *line, int lim)
+{
+    int i;
+    char *ptr = line;
+    int l_brace = 0;
+
+    for (i = 0; i < lim-1 && *ptr; i++) {
+        if (*ptr == '@')
+            return A_COMMAND;
+        else if (*ptr == '(')
+            l_brace = 1;
+        else if (*ptr == ')' && l_brace)
+            return L_COMMAND;
+        ptr++;
+    }
+    return C_COMMAND;
+}
+
 bool contains(char *label)
 {
     int i;
     for (i = 0; i < ENTRYNUM; i++) {
         //printf("compare %s\n", symboltab[j].label);
         if (strncmp(label, symboltab[i].label, strlen(label)-1) == 0) { // ignore the null terminator, it might be '\n' or EOF
-            printf("Has set up.\n");
+            //printf("Has set up.\n");
             return true;
         }
     }
@@ -84,6 +116,199 @@ int getAddress(char *symbol)
     return -1;
 }
 
+void printTable()
+{
+    int j;
+    for (j = 0; j < ENTRYNUM; j++) {
+        if (isalnum(symboltab[j].label[0])) {
+            printf("T: %d, %s\n", symboltab[j].address, symboltab[j].label);
+        }
+    }
+}
+
+// 16 -> 0000000000010000
+char * convertA(unsigned int in, char *out)
+{
+    unsigned int mask = 1U << (BITS_NUM-1);
+    int i;
+    for (i = 0; i < BITS_NUM; i++) {
+        out[i] = (in & mask) ? '1' : '0';
+        in <<= 1;
+    }
+    return out;
+}
+
+char * convertDest(char *src)
+{
+    char * desttab[DESTTAB_LEN] = {
+        "null", "M", "D", "MD", "A", "AM", "AD", "AMD"
+    };
+    char * destBintab[DESTTAB_LEN] = {
+        "000", "001", "010", "011", "100", "101", "110", "111"
+    };
+    int i;
+    for (i = 0; i < DESTTAB_LEN; i++) {
+        if (strcmp(src, desttab[i]) == 0)
+            return destBintab[i];
+    }
+    return "000";
+}
+
+// if a equal 1, then the c-converter should know that a bit set to one
+char * convertComp(char *src, int len, int * a)
+{
+    char * comptab[COMPTAB_LEN] = {
+        "0", "1", "-1", "D",
+        "A", "!D", "!A", "-D",
+        "-A", "D+1", "A+1", "D-1",
+        "A-1", "D+A", "D-A", "A-D",
+        "D&A", "D|A"
+    };
+    char * compBintab[COMPTAB_LEN] = {
+        "101010", "111111", "111010", "001100",
+        "110000", "001101", "110001", "001111",
+        "110011", "011111", "110111", "001110",
+        "110010", "000010", "010011", "000111",
+        "000000", "010101"
+    };
+    // check a bit, if M existed, then set it to true
+    *a = 0;
+    int i;
+    char *ptr = src;
+    for(i = 0; i < len; i++) {
+        if (*ptr == 'M') {
+            *a = 1;
+            *ptr = 'A';
+        }
+        ptr++;
+    }
+    //printf("a: %d, src: %s\n", *a, src);
+
+    // after check the a bit, no matter A/M used in src, turn A/M in src to A
+    for (i = 0; i < COMPTAB_LEN; i++) {
+        if (strcmp(src, comptab[i]) == 0)
+            return compBintab[i];
+    }
+
+    // should not come here
+    return "error";
+}
+
+char * convertJMP(char *src)
+{
+    char * jumptab[8] = {
+        "null", "JGT", "JEQ", "JGE", "JLT", "JNE", "JLE", "JMP"
+    };
+    char * jumbBintab[8] = {
+        "000", "001", "010", "011", "100", "101", "110", "111"
+    };
+    int i;
+    for (i = 0; i < JUMPTAB_LEN; i++) {
+        if (strcmp(src, jumptab[i]) == 0)
+            return jumbBintab[i];
+    }
+    return "000";
+}
+
+// the start of c-instruction must be 111
+// ex: 111accccccdddjjj
+// a is decided inside the comp bits,
+// if a==1, then ALU use M
+// if a==0, then ALU use A-register
+char * convertC(char *instruction)
+{
+    int has_tok_1 = 0,
+        has_tok_3 = 0;
+    int i = 0, j = 0;
+    char c;
+    char *token_1 = malloc(sizeof(char) * TOK_LEN),
+         *token_2 = malloc(sizeof(char) * TOK_LEN),
+         *token_3 = malloc(sizeof(char) * TOK_LEN);
+    int check_a;
+    printf("instruction: %s\n", instruction);
+    // remember the index of the equal symbol
+    // if the index is zero, then we know that token_1 or token_3
+    // should be skipped
+    while(((c = instruction[i++]) && c != '=') && i < BITS_NUM)
+        ;
+    if (c == '=')
+        has_tok_1 = i-1;
+    i = 0;
+    while(((c = instruction[i++]) && c != ';') && i < BITS_NUM)
+        ;
+    if (c == ';')
+        has_tok_3 = i-1;
+    //printf("= : %d\n", has_tok_1);
+    //printf("; : %d\n", has_tok_3);
+
+    i = 0;
+    if (has_tok_1) {
+        while(has_tok_1 > i) {
+            token_1[i] = instruction[i];
+            i++;
+        }
+        token_1[i] = '\0';
+    } else {
+        token_1[0] = '\0';
+    }
+    printf("token_1 %s\n", token_1);
+
+    // computer the start position of token_2
+    i = has_tok_1;
+    if (has_tok_1)
+        i++;
+    j = 0;
+    //printf("i %d\n", i);
+    if (has_tok_3) {
+        while(i < has_tok_3 && i < BITS_NUM)
+            token_2[j++] = instruction[i++];
+        if (!has_tok_3)
+            token_2[0] = '\0';
+        else
+            token_2[j] = '\0';
+    } else {
+        while(i < BITS_NUM)
+            token_2[j++] = instruction[i++];
+        token_2[j] = '\0';
+    }
+
+    // token part 3
+    i++;
+    j = 0;
+    if (has_tok_3) {
+        while(instruction[i] && i < BITS_NUM) {
+            token_3[j++] = instruction[i++];
+        }
+        token_3[j] = '\0';
+    } else {
+        token_3[0] = '\0';
+    }
+    printf("token_2 %s, len %lu\n", token_2, strlen(token_2));
+    printf("token_3 %s\n", token_3);
+
+    // turn to binary
+    char *dest = convertDest(token_1);
+    char *comp = convertComp(token_2, strlen(token_2), &check_a);
+    char *jump = convertJMP(token_3);
+    //printf("a-bit: %d\n", check_a);
+    //printf("comp: %s\n", comp);
+    //printf("dest: %s\n", dest);
+    //printf("jump: %s\n", jump);
+
+    // concat all
+    char *result = malloc(sizeof(char) * (BITS_NUM+1));
+    strcpy(result, "111");
+    if (check_a)
+        strcat(result, "1");
+    else
+        strcat(result, "0");
+    strcat(result, comp);
+    strcat(result, dest);
+    strcat(result, jump);
+    //printf("result: %s\n", result);
+
+    return result;
+}
 
 int main(int argc, const char *argv[])
 {
@@ -93,6 +318,12 @@ int main(int argc, const char *argv[])
         printf("usage: ./a.out <filename>\n");
     else {
         FILE *fp = fopen(argv[1], "r");
+        strcpy(buffer, argv[1]);
+        char *writefile = buffer;
+        while(*writefile++ != '.') ;
+        strcpy(writefile, "hack");
+        FILE *fw = fopen(buffer, "w");
+
         char *lineptr = buffer;
         size_t linecap = BUFFERLEN;
         int linenum = 0;
@@ -126,9 +357,9 @@ int main(int argc, const char *argv[])
                 }
             }
         }
+
         fseek(fp, 0, SEEK_SET);
 
-        // parse symbolic a-instruction
         while((i = getline(&lineptr, &linecap, fp)) > 0) {      // Pass 2
             char command[linecap];
             char *ptr = buffer;
@@ -137,30 +368,34 @@ int main(int argc, const char *argv[])
             if (*ptr == '/')
                 continue;
             int j = 0;
-            while(j < i) {
-                command[j++] = *ptr++;
-                if (*ptr == ' ' || *ptr == '\n')
-                    break;
+            for(; j < i && *ptr != ' ' && *ptr != '\n'; j++, ptr++) {
+                command[j] = *ptr;
             }
             command[j] = '\0';
+            if (!isalnum(command[j-1]))
+                command[j-1] = '\0';
+            printf("command: %s\n", command);
             if (strlen(command) > 1) {      // above did not filter the newline, and if there is, just skip it
                 if (command[0] == '@') {
                     if (!isdigit(command[1])) {
                         char *label = &command[1];
                         if (!contains(label)) {
                             addEntry(label, registerptr++);
-                            printf("add: %s\n", label);
+                        } else {
+                            char out[50];
+                            printf("AAA %s\n", convertA(getAddress(label), out));
+                            fputs(convertA(getAddress(label), out), fw);
+                            fputs("\n", fw);
                         }
                     }
+                } else if (command[0] != '(') {        // handle instruction c
+                    printf("%s\n", convertC(command));
+                    fputs(convertC(command), fw);
+                    fputs("\n", fw);
                 }
             }
         }
     }
-    int j;
-    for (j = 0; j < ENTRYNUM; j++) {
-        if (isalnum(symboltab[j].label[0])) {
-            printf("T: %d, %s\n", symboltab[j].address, symboltab[j].label);
-        }
-    }
+    printTable();
     return 0;
 }
